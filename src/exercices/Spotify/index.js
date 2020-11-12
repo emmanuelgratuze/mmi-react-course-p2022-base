90ed228c6fc25a2392906da7315a2ca1f222bd09
Emmanuel Gratuze <emmanuel.gratuze@gmail.com>
2020-11-12 16:59:48 +0100

    Add Spotify example

--- src/exercices/Spotify/index.js
+++ src/exercices/Spotify/index.js
@@ -1,13 +1,25 @@
-// import React from 'react'
-// import SpotifyProvider from 'components/SpotifyProvider'
+import React from 'react'
+import SpotifyProvider from 'components/SpotifyProvider'
+import Tracks from './Tracks'
 
 // import Player from './Player'
 
-// const spotifyClientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID
-// const spotifyRedirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI
+const spotifyClientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+const spotifyRedirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI
 
 const Spotify = () => {
-  return "Spotify youhou!"
+  return (
+    <div>
+      <h1>Spotify</h1>
+
+      <SpotifyProvider
+        clientId={spotifyClientId}
+        redirectUri={spotifyRedirectUri}
+      >
+        <Tracks />
+      </SpotifyProvider>
+    </div>
+  )
 }
 
 export default Spotify
